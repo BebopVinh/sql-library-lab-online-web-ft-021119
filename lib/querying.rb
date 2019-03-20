f2058af8d5a1b04de8c4b50abb2b375d25a8173e
Vinh Do <vinhdo07@gmail.com>
2019-03-20 04:01:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,10 +31,12 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  FROM series
-  JOIN characters
-  ON characters.series_id = series.id
-  WHERE characters.species = 'human'"
+FROM series
+JOIN characters
+ON characters.series_id = series.id
+WHERE characters.species = 'human'
+ORDER BY series.title LIMIT 1
+	"
 end
 
 def select_character_names_and_number_of_books_they_are_in
