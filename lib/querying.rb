56a451c57b55e683761419bcb3a1d3ff3babc188
Vinh Do <vinhdo07@gmail.com>
2019-03-20 04:04:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,8 +35,7 @@ FROM series
 JOIN characters
 ON characters.series_id = series.id
 WHERE characters.species = 'human'
-ORDER BY series.title LIMIT 1
-	"
+ORDER BY series.title LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
